dbc7e24f16ef491e4f3cc1da25112b1d7670b771
Guilhem <guilhemlemouel@gmail.com>
2025-08-28 20:06:59 +0100

    inti

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Sprout } from 'lucide-svelte'
   import { pb, getCurrentUser } from './pocketbase.svelte'
 
   let email = $state('')
@@ -19,10 +20,22 @@
   let currentUser = $derived.by(getCurrentUser)
 </script>
 
+<style>
+  .logo {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+  }
+</style>
+
 {#if currentUser}
   <p>Logged in as {currentUser.name}</p>
   <button type="button" onclick={logout}>Logout</button>
 {:else}
+  <div class="logo">
+    <Sprout size={48} color="#4CAF50" />
+  </div>
+  <h1>Welcome to the garden !</h1>
   <form onsubmit={(e) => e.preventDefault()} class="flex flex-col gap-4">
     <input type="text" name="email" placeholder="Email" bind:value={email} />
     <input type="password" name="password" placeholder="Password" bind:value={password} />
