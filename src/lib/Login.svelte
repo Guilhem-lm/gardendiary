12865df63e46095c179991b2bf850784d12b15f4
Guilhem <guilhemlemouel@gmail.com>
2025-08-28 20:51:18 +0100

    base

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { Sprout } from 'lucide-svelte'
+  import { LogOut, Sprout } from 'lucide-svelte'
   import { pb, getCurrentUser } from './pocketbase.svelte'
+  import Container from './Containers.svelte'
 
   let email = $state('')
   let password = $state('')
@@ -29,18 +30,29 @@
 </style>
 
 {#if currentUser}
-  <div class="flex flex-col gap-2">
-    <p>Logged in as {currentUser.name}</p>
-    <button type="button" onclick={logout}>Logout</button>
+  <div class="flex flex-col h-ful w-full justify-start">
+    <!-- Top bar -->
+    <div class="flex justify-between gap-2 items-center w-full h-10 px-4">
+      <div class="flex items-center gap-2">
+        <Sprout size={24} color="#4CAF50" />
+      </div>
+      <button type="button" onclick={logout} class="text-sm">
+        <LogOut size={16} />
+      </button>
+    </div>
+    <Container />
   </div>
 {:else}
-  <div class="logo">
-    <Sprout size={48} color="#4CAF50" />
+  <div class="flex flex-col h-full w-full justify-center items-center">
+    <div class="logo">
+      <Sprout size={48} color="#4CAF50" />
+    </div>
+    <div class="flex flex-col gap-4">
+      <form onsubmit={(e) => e.preventDefault()} class="flex flex-col gap-4">
+        <input type="text" name="email" placeholder="Email" bind:value={email} />
+        <input type="password" name="password" placeholder="Password" bind:value={password} />
+        <button type="submit" onclick={login}>Login</button>
+      </form>
+    </div>
   </div>
-  <h1>Welcome to the garden !</h1>
-  <form onsubmit={(e) => e.preventDefault()} class="flex flex-col gap-4">
-    <input type="text" name="email" placeholder="Email" bind:value={email} />
-    <input type="password" name="password" placeholder="Password" bind:value={password} />
-    <button type="submit" onclick={login}>Login</button>
-  </form>
 {/if}
